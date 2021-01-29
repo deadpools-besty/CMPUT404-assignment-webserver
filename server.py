dbc1e600f5122758dde0e39d47e60455ddb011e2
Nana Andoh <nanaegyir2000@hotmail.com>
2021-01-29 04:12:08 -0700

    eureka?

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 #  coding: utf-8 
+from genericpath import exists
 import socketserver
-
+import os
+import http.server
+from sys import path
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,11 +31,82 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
+
+    def fetch_file(self, requested_file):
+        fname = 'www' + requested_file
+        try:
+            f = open(fname, "r")
+            lines = f.readlines()
+            f.close()
+            return lines
+        except Exception as e:
+            return str(e)
+    
+    def find_file_type(self, filename):
+        return filename.split('.')[1]
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        print ("Got a request of: %s\n" % str(self.data, 'utf-8'))
+        
+        encoded_data = str(self.data, "utf-8")
+
+        list_of_encoded = encoded_data.split('\r')
+        
+        # list of not allowed methods for producing 405 error
+        na_method_list = ['POST', 'PUT', 'DELETE']
+        
+        # compare against the method list to see if method is allowed
+        request_type = list_of_encoded[0].split()[0]
+        requested_file = list_of_encoded[0].split()[1]
+        
+        # handle 405 error
+        if request_type in na_method_list:
+            self.request.sendall(bytearray('HTTP/1.1 405 Method not allowed\r\nConnection: close\r\n', "utf-8"))
+        
+        # check if dir exists
+        elif os.path.exists('www' + requested_file):
+            
+            # handle user inputting just a directory
+            if os.path.isdir('www' + requested_file):
+                
+                if requested_file[-1] == '/':
+                
+                    readout = self.fetch_file(requested_file + 'index.html')
+                    
+                    readout_string = ''
+                    
+                    for line in readout:
+                        readout_string += line
+                    bytearray_to_send = f'''HTTP/1.1 200 Ok\r\nContent-Type: text/html; charset=utf-8\r\nConnection: close\r\n\r\n{readout_string}'''
+                    self.request.sendall(bytearray(bytearray_to_send,'utf-8'))
+                else:
+                    # handle 301 and redirect
+                    bytearray_to_send = f'''HTTP/1.1 301 Permanently moved\r\nLocation: {requested_file + '/'}\r\nConnection: close\r\n\r\n'''
+                    self.request.sendall(bytearray(bytearray_to_send,'utf-8'))
+    
+            # handle user inputting file
+            elif os.path.isfile('www' + requested_file):
+                readout = self.fetch_file(requested_file)
+                
+
+                # generate string to send 
+                readout_string = ''
+                for line in readout:
+                    readout_string += line
+
+                # send response
+                file_ext = self.find_file_type(requested_file)
+                bytearray_to_send = f'''HTTP/1.1 200 Ok\r\nContent-Type: text/{file_ext}; charset=utf-8\r\nConnection: close\r\n\r\n{readout_string}'''
+                self.request.sendall(bytearray(bytearray_to_send,'utf-8'))
+                    
+        elif not os.path.exists('www' + requested_file):
+            bytearray_to_send = f'''HTTP/1.1 404 Not found\r\nConnection: close\r\n'''
+            self.request.sendall(bytearray(bytearray_to_send, 'utf-8'))
+            
+            
+                    
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
